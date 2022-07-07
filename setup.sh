51123b1a4d18e6c4448434f7bf3bbf9db99c9bbe
kwagga <janroot@gmail.com>
2022-07-07 17:46:42 +0200

    Added breaks between stages

--- setup.sh
+++ setup.sh
@@ -21,42 +21,58 @@ install_and_config() {
     sudo apt-add-repository -y "deb https://apt.releases.hashicorp.com $(lsb_release -cs) main"
     sudo apt-get install -qq vault-enterprise-hsm softhsm2 opensc
     sudo systemctl enable vault.service
+    read -p "Press any key to continue... " -n1 -s
 
     # Add vault user to softhsm group
+    clear
     echo_green "Adding vault user to softhsm group"
     echo "sudo adduser vault softhsm"
     sudo adduser vault softhsm
+    read -p "Press any key to continue... " -n1 -s
 
     # Let's show the SoftHSM slots
+    clear
     echo_green "Listing SoftHSM slots (pre-config)"
     echo "sudo softhsm2-util --show-slots"
     sudo softhsm2-util --show-slots
+    read -p "Press any key to continue... " -n1 -s
 
     # Need to configure a new slot for Vault
+    clear
     echo_green "Configuring a new slot for Vault"
     echo "sudo -u vault softhsm2-util --init-token --slot 0 --label "vault_hsm_key" --pin 1234 --so-pin asdf"
     sudo -u vault softhsm2-util --init-token --slot 0 --label "vault_hsm_key" --pin 1234 --so-pin asdf
+    read -p "Press any key to continue... " -n1 -s
 
     # Show new slot
+    clear
     echo_green "Listing SoftHSM slots (post-config) using softhsm2-util"
     echo "sudo softhsm2-util --show-slots"
     sudo softhsm2-util --show-slots
+    read -p "Press any key to continue... " -n1 -s
 
     # Show new slot using pkcs#11
+    clear
     echo_green "Listing SoftHSM slots (post-config) using pkcs#11"
     echo "sudo pkcs11-tool --module /usr/lib/softhsm/libsofthsm2.so -L"
     sudo pkcs11-tool --module /usr/lib/softhsm/libsofthsm2.so -L
+    read -p "Press any key to continue... " -n1 -s
 
     # Gather the new slot number and place this in VAULT_HSM_SLOT
+    clear
     echo_green "Reading and saving the slot number to VAULT_HSM_SLOT"
     echo "VAULT_HSM_SLOT=\$(sudo softhsm2-util --show-slots | grep \"^Slot \" | head -1 | cut -d \" \" -f 2)"
     VAULT_HSM_SLOT=$(sudo softhsm2-util --show-slots | grep "^Slot " | head -1 | cut -d " " -f 2)
+    echo "VAULT_HSM_SLOT="$VAULT_HSM_SLOT
+    read -p "Press any key to continue... " -n1 -s
 
     # Setting VAULT_ADDR env variable
+    clear
     echo_green "Setting VAULT_ADDR environment variable ..."
     echo "export VAULT_ADDR=\"http://127.0.0.1:8200\""
     echo export VAULT_ADDR="http://127.0.0.1:8200" >> ~/.profile
     export VAULT_ADDR="http://127.0.0.1:8200"
+    read -p "Press any key to continue... " -n1 -s
 
     # Add Vault license
     echo_green "Adding license ..."
@@ -64,6 +80,7 @@ install_and_config() {
     echo "VAULT_LICENSE_PATH=/etc/vault.d/vault.hclic" | sudo tee -a /etc/vault.d/vault.env
 
     # Setup Vault config for HSM etc...
+    clear
     echo_green "Setting new Vault configuration ..."
     sudo mv /etc/vault.d/vault.hcl /etc/vault.d/vault_hcl.old
 
@@ -93,15 +110,18 @@ generate_key   = "true"
 EOF
 
     sudo chown -R vault:vault /etc/vault.d/*
+    read -p "Press any key to continue... " -n1 -s
 
     # Start Vault
+    clear
     echo_green "Starting Vault"
     sudo systemctl start vault.service
-
+    read -p "Press any key to continue... " -n1 -s
     # Check if Vault is up
 
     STATUS="$(systemctl is-active vault.service)"
     if [ "${STATUS}" = "active" ]; then
+        clear
         echo_green "Vault is running, let's initialize....."
         echo_green "Initializing Vault and exporting keys to `echo ~`/unseal.keys - NEVER do this in production"
         echo "vault operator init -recovery-shares=1 -recovery-threshold=1 >> ~/unseal.keys"
